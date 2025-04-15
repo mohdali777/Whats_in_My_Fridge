2e13219d9a29cd43a4ec13d4656b1373b1a1be52
mohdali777 <alimohd87654321al@gmail.com>
2025-04-15 18:51:40 +0530

    n

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from '../../assets/logo.png'
+export default function Navbar() {
+  return (
+    <div>
+      <div>
+       <img src={logo} alt="" />
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
